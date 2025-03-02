5d4ca4d61dd75ee52d157a6605e3a2b51189f24d
TheSimmer101 <rachelbriskman@gmail.com>
2025-03-02 17:43:17 -0500

    made some improvements

--- angular-fasion-app/src/app/app.component.ts
+++ angular-fasion-app/src/app/app.component.ts
@@ -4,44 +4,8 @@ import { User } from '@angular/fire/auth';
 import { CommonModule } from '@angular/common';
 import { subscribeOn } from 'rxjs';
 
-// Stuff I added for database:
 import { initializeApp } from "firebase/app";
-import { getDatabase,ref,set } from "firebase/database";
-const firebaseConfig = {
-  // ...
-  // The value of `databaseURL` depends on the location of the database
-  databaseURL: "https://fashion-project-e2aba-default-rtdb.firebaseio.com/:null",
-};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// Initialize Realtime Database and get a reference to the service
-const database = getDatabase(app);
-
-// function writeUserData(uid, name, email, imageUrl) {
-//   set(ref(database, 'users/' + uid), {
-//     username: name,
-//     email: email,
-//     profile_picture : imageUrl
-//   });
-//}
-type Category = 'Tops' | 'Bottoms' | 'Shoes' | 'Hats' | 'Glasses' | 'Earrings' | 'Necklaces' | 'Bracelets' | 'Watches' | 'Rings';
-function addClothes(itemID: number, uid: number, itemName: string, itemDesc: string, image_url: string, imageBlob: Blob, category: Category) {
-  set(ref(database, 'Clothes/' + itemID), {
-    UID: uid,
-    Item_Name: itemName,
-    Item_Desc: itemDesc,
-    Image_url: image_url,
-    Image_Blob: imageBlob,
-    Category: category
-  });
-
-  // Function to add outfits data
-function addOutfit(outfitID: number, outfitName: string, items: { [key: string]: boolean }) {
-  set(ref(database, 'Outfits/' + outfitID), {
-    Outfit_Name: outfitName,
-    Items: items
-  });
 @Component({
   selector: 'app-root',
   standalone: true,
