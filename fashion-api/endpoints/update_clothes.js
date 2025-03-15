07d24394327f86aa1f9d9b6f86b734df07df8393
TheSimmer101 <rachelbriskman@gmail.com>
2025-03-15 00:44:03 -0400

    Did the Update API

--- fashion-api/endpoints/update_clothes.js
+++ fashion-api/endpoints/update_clothes.js
@@ -39,7 +39,53 @@ app.listen(port, () => {
 app.put('/:user_id/inventory/:clothing_id', (req, res) => {
     res.send('PUT request to update clothing information')
 
-    const clothing_id = req.params.clothing_id;
 
-    console.log(`Successfully updated clothes`)
-})
+    const clothing_id = req.params.clothing_id;
+    const clothingRef = database.ref(`users/${user_id}/clothing/${clothing_id}`); 
+    const changes = req.body;
+    const validCategories = [
+        'Tops', 'Bottoms', 'Shoes', 'Hats',
+        'Glasses', 'Earrings', 'Necklaces',
+        'Bracelets', 'Watches', 'Rings'
+      ];
+    //clothes details
+    // UID: uid,
+    // Item_Name: itemName,
+    // Item_Desc: itemDesc,
+    // //Image_url: image_url,
+    // Image_Blob: imageBlob,
+    // Category: category
+    //Category = 'Tops' | 'Bottoms' | 'Shoes' | 'Hats' | 'Glasses' | 'Earrings' | 'Necklaces' | 'Bracelets' | 'Watches' | 'Rings';
+
+    if (changes.Category)
+        {
+            if (!validCategories.includes(updates.category)) {
+                return res.status(400).send({ error: 'Invalid category value!' });
+              }
+        }  
+    else if(changes.item_name)
+        {
+            if(typeof changes.item_name != 'string')
+                return res.status(400).send({ error: 'Item Name must be a string!' });
+        }
+    else if(changes.item_desc)
+        {
+            if(typeof changes.item_desc != 'string')
+                return res.status(400).send({ error: 'Item Description must be a string!' });
+        }
+    else if (changes.image_blob) 
+        {
+            // Check if it's a valid base64-encoded string
+            if (!changes.image_blob.startsWith('data:image/')) {
+              return res.status(400).send({ error: 'Invalid image format!' });
+            }
+        }
+    clothingRef.update(changes)
+    .then(() => {
+      res.status(200).send({ message: 'Data updated successfully!', updatedFields: changes });
+    })
+    .catch(error => {
+      res.status(500).send({ error: 'Failed to update data', details: error.message });
+    });
+    console.log(`Successfully updated clothes`);
+    })
